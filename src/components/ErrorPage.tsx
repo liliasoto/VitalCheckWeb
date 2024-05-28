39ef0d3353ffcf820bee1db9ad4bd47aa90874c8
liliasoto <0.lilia.soto.0@gmail.com>
2024-05-27 22:35:21 -0600

    Estilos de pagina de error

--- src/components/ErrorPage.tsx
+++ src/components/ErrorPage.tsx
@@ -1,12 +1,13 @@
 import { useNavigate } from "react-router-dom";
+import '../estilos/ErrorPage.css';
 
-function ErrorPage(){
+function ErrorPage() {
     const navigate = useNavigate();
-    return(
-        <div>
-            <h1>Sorry, this page doesn't exist!!</h1>
-            <button onClick={()=>navigate("/")}>
-                Go to main page
+    return (
+        <div className="error-container">
+            <h1>Perdón, esta página no existe :(</h1>
+            <button onClick={() => navigate("/")}>
+                Ir a home
             </button>
         </div>
     );
